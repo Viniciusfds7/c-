e7ccd7b9d78bc3973a8488faae1c3f8d05d4ba4b
Pc_Casa <viniciusfds7@gmail.com>
2019-01-22 22:26:04 -0200

    Jogabilidade do jogo
    adiciona letra
    tentativas
    é aleatório
    
    tem um erro na contagem de letra repetidas

--- Jogo da forca/Jogo da forca.cpp
+++ Jogo da forca/Jogo da forca.cpp
@@ -12,28 +12,49 @@ void limpatela () {
 
 string retornapalavraaleatoria (){
 
-    string palavras[3] = ("Abacaxi", "Manga", "Morango");
+    string palavras[5] = {"abacaxi", "manga", "morango", "banana", "damasco"};
 
     //Indice gerado pelo intervalo [0,1,2]
-    int indiceAleatorio = rand() % 3;
+    int indicealeatorio = rand() %5 ;
+
+    cout << indicealeatorio;
+
+    system("pause");
 
     //Exibe uma palavra aleatória
-    return palavras[indiceAleatorio];
+    return palavras[indicealeatorio];
 
 }
 
 string retornapalavracommascara(string palavra, int tamanhodapalavra){
-int cont = 0;
+    int cont = 0;
     string palavracommascara;
 
 
     //Coloca mascara
     while(cont < tamanhodapalavra){
-        palavracommascara += "_ ";
+        palavracommascara += "_";
         cont++;
     }
 
     return palavracommascara;
+}
+
+void  exibestatus(string palavracommascara, int tamanhodapalavra, int tentativasrestantes, string letrasjaarriscadas) {
+
+    //cout << "A palavra secreta eh: "<< palavra << "(Tamanho: "<< tamanhodapalavra <<")\n\n" ;
+    cout << "Palavra "<< palavracommascara << "(Tamanho: "<< tamanhodapalavra <<")\n\n" ;
+    cout << "\nTentativas restantes: " << tentativasrestantes << "\n\n\n";
+
+
+    //Exibe as letras arriscadas
+    int cont;
+    cout << "\nLetras Arriscadas: ";
+    for(cont = 0; cont < letrasjaarriscadas.size(); cont++){
+        cout << letrasjaarriscadas[cont] << ", ";
+    }
+
+
 }
 
 
@@ -48,8 +69,76 @@ void jogasozinho(){
     //Palavra mascarada
     string palavracommascara = retornapalavracommascara(palavra, tamanhodapalavra);
 
-    cout << "A palavra secreta eh: "<< palavra << "(Tamanho: "<< tamanhodapalavra <<")\n\n" ;
-    cout << "\nMascara:" << palavracommascara << "\n\n\n";
+    ///Variáveis pricipais
+    int tentativas = 0, maximodetentativas = 7;       //números de tentativas
+    int cont = 0;                                    //auxiliar ocntadora
+    char letra;                                      //letra arriscada
+    string letrasjaarriscadas;                       //acumula as tentativas do jogador
+    bool jadigitouletra = false;                             //auxiliar para letra já digitada
+
+
+    while (palavra != palavracommascara && (maximodetentativas - tentativas) > 0) {
+
+
+        //limpatela();
+
+        //exibe o status atual do jogo
+        exibestatus(palavracommascara,tamanhodapalavra, maximodetentativas - tentativas, letrasjaarriscadas);
+
+        //Palpites
+        cout << "\nDigite uma letra: ";
+        cin >> letra;
+
+
+        //percorre as letras já arriscadas
+        for (cont=0; cont<tentativas; cont++){
+
+            //Se encontra a letra
+            if(letrasjaarriscadas[cont] == letra){
+
+                cout << "\nEssa letra já foi digitada.\n";
+                //Indice com a variavel booleana
+                jadigitouletra=true;
+            }
+        }
+
+        if (jadigitouletra==false){
+                letrasjaarriscadas += letra;
+
+
+        //percorre a palavra real e se existir muda a palavra
+        for(cont=0; cont<tamanhodapalavra; cont++){
+
+            // Se a letra existir na palavra escondida
+            if(palavra[cont]==letra){
+
+                //Faço a letra aparecer na palavra
+                palavracommascara[cont] = palavra[cont];
+            }
+
+        }
+
+        //aumenta as tentativas
+        tentativas++;
+
+        }
+
+
+
+
+
+    }
+
+if (palavra == palavracommascara){
+
+    limpatela();
+    cout << "\n\n\nPARABENS!! VOCE VENCEU O MELHOR JOGO DO MUNDO. MUITO FELIZ POR VOCE. VOCE EH UM GENIO.\n\n\n";
+    system("pause");
+} else {
+    limpatela();
+    cout << "\n\n\nInfelizmente voce perdeu. Tente novamente.\n\n\n";
+    system("pause");
+}
 
 }
 
@@ -69,6 +158,7 @@ void menuinicial (){
     cout <<"\n2 - Sobre";
     cout <<"\n3 - Sair";
     cout <<"\nEscolha uma opcao e tecle ENTER:";
+
 
     //leitura da opcao
     cin >> opcao;
@@ -101,9 +191,11 @@ void menuinicial (){
 
 int main(){
 
-    srand[(unsigned) time(NULL)];
+    srand((unsigned) time(NULL));
 
     menuinicial();
+
+
 
 
 
